73a0fb57b530d9e67ac20751bcd48a413e85423c
Harvey Nixon <hnixon@kispe.co.uk>
2024-07-29 11:38:40 +0100

    Create button_press.ino

---
+++ button_press/button_press.ino
@@ -0,0 +1,40 @@
+
+bool state = false;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+
+  Serial.println("------------------------------------------------"); 
+  Serial.println();
+  Serial.println("  _  _______  _____ _____  ______ ");
+  Serial.println(" | |/ /_   _|/ ____|  __ \\|  ____|");
+  Serial.println(" | ' /  | | | (___ | |__) | |__   ");
+  Serial.println(" |  <   | |  \\___ \\|  ___/|  __|  ");
+  Serial.println(" | . \\ _| |_ ____) | |    | |____ ");
+  Serial.println(" |_|\\_\\_____|_____/|_|    |______|");
+  Serial.println();
+  Serial.println("------------------------------------------------");
+  delay(2000);
+
+  pinMode(USER_BTN, INPUT);
+  // USER_BTN is pin PC13
+
+
+}
+
+void loop() {
+  state = digitalRead(USER_BTN);
+
+  if (state == false){
+
+    Serial.println("LOW");
+
+  } else if (state == true){
+
+    Serial.println("HIGH");
+
+  }
+
+  delay(1);
+}
