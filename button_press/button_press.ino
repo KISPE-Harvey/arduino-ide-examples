ddc70a62d59e68af0fc7c1759b25c6883d86db1d
Harvey Nixon <hnixon@kispe.co.uk>
2024-07-29 16:23:53 +0100

    Update button_press.ino

--- button_press/button_press.ino
+++ button_press/button_press.ino
@@ -1,5 +1,23 @@
+/*
+  _  _______  _____ _____  ______ 
+ | |/ /_   _|/ ____|  __ \|  ____|
+ | ' /  | | | (___ | |__) | |__   
+ |  <   | |  \___ \|  ___/|  __|  
+ | . \ _| |_ ____) | |    | |____ 
+ |_|\_\_____|_____/|_|    |______|                                 
+   _____ _____        _____ ______ 
+  / ____|  __ \ /\   / ____|  ____|
+ | (___ | |__) /  \ | |    | |__   
+  \___ \|  ___/ /\ \| |    |  __|  
+  ____) | |  / ____ \ |____| |____ 
+ |_____/|_| /_/    \_\_____|______|                         
 
+  Development and test code for the STM32H753ZIT uC
+
+  Last updated 29/07/2024 Harvey Nixon
+*/
 bool state = false;
+// This variable shows the state of the button, true if pressed, false if not
 
 void setup() {
   Serial.begin(9600);
@@ -24,16 +42,18 @@ void setup() {
 }
 
 void loop() {
+  
   state = digitalRead(USER_BTN);
+  // Read if the buton is pressed or not
 
   if (state == false){
+    // If button is not pressed then:
 
     Serial.println("NOT PRESSED");
-
   } else if (state == true){
+    // If button is pressed then:
 
     Serial.println("PRESSED");
-
   }
 
   delay(1);
