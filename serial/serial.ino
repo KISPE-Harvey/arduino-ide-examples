f2063a89505d8af225b422aefd667a618b361bcc
Harvey Nixon <hnixon@kispe.co.uk>
2024-07-29 09:35:29 +0100

    Updated serial example

--- serial/serial.ino
+++ serial/serial.ino
@@ -6,6 +6,17 @@ void setup() {
   Serial.begin(9600);
   delay(2000);
 
+  Serial.println("------------------------------------------------"); 
+  Serial.println();
+  Serial.println("  _  _______  _____ _____  ______ ");
+  Serial.println(" | |/ /_   _|/ ____|  __ \|  ____|");
+  Serial.println(" | ' /  | | | (___ | |__) | |__   ");
+  Serial.println(" |  <   | |  \___ \|  ___/|  __|  ");
+  Serial.println(" | . \ _| |_ ____) | |    | |____ ");
+  Serial.println(" |_|\_\_____|_____/|_|    |______|");
+  Serial.println();
+  Serial.println("------------------------------------------------");
+
 }
 
 void loop() {
