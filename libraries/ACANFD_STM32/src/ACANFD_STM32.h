36a71d0340c6f00fe7e4edfa885c4a4a1bd1c687
Harvey Nixon <hnixon@kispe.co.uk>
2024-08-02 16:39:00 +0100

    update CAN FD

---
+++ libraries/ACANFD_STM32/src/ACANFD_STM32.h
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------------------------
+
+#pragma once
+
+//----------------------------------------------------------------------------------------
+
+#ifdef ARDUINO_NUCLEO_H743ZI2
+  #include "ACANFD_STM32_NUCLEO_H743ZI2-objects.h"
+#elif defined (ARDUINO_NUCLEO_G431KB)
+  #include "ACANFD_STM32_NUCLEO_G431KB-object.h"
+#elif defined (ARDUINO_NUCLEO_G474RE)
+  #include "ACANFD_STM32_NUCLEO_G474RE-objects.h"
+#elif defined (ARDUINO_NUCLEO_H753ZI)
+  //#include "ACANFD_STM32_NUCLEO_H753ZI-objects.h" 
+  #include "ACANFD_STM32_NUCLEO_H743ZI2-objects.h" 
+#else
+  #error "Unhandled Nucleo Board"
+#endif
+
+//----------------------------------------------------------------------------------------
