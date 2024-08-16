02befda629c63f7110dcc398fa26c370f7531bb6
Harvey Nixon <hnixon@kispe.co.uk>
2024-08-16 11:55:53 +0100

    save

---
+++ SATLL/STM32H7VIT/can_read/can_read.ino
@@ -0,0 +1,95 @@
+/*
+  _  _______  _____ _____  ______ 
+ | |/ /_   _|/ ____|  __ \|  ____|
+ | ' /  | | | (___ | |__) | |__   
+ |  <   | |  \___ \|  ___/|  __|  
+ | . \ _| |_ ____) | |    | |____ 
+ |_|\_\_____|_____/|_|    |______|                                 
+   _____ _____        _____ ______ 
+  / ____|  __ \ /\   / ____|  ____|
+ | (___ | |__) /  \ | |    | |__   
+  \___ \|  ___/ /\ \| |    |  __|  
+  ____) | |  / ____ \ |____| |____ 
+ |_____/|_| /_/    \_\_____|______|                         
+
+  Development and test code for the STM32H753ZIT uC
+
+  Last updated 06/08/2024 Harvey Nixon
+*/
+#define CAN1_TX_PIN PA12
+#define CAN1_RX_PIN PA11
+
+// #define CAN2_TX_PIN PB13
+// #define CAN2_RX_PIN PB12
+
+static const uint32_t FDCAN1_MESSAGE_RAM_WORD_SIZE = 2560 ;
+static const uint32_t FDCAN2_MESSAGE_RAM_WORD_SIZE = 0 ; // FDCAN2 not used
+
+// static const uint32_t FDCAN1_MESSAGE_RAM_WORD_SIZE = 0 ;// FDCAN1 not used
+// static const uint32_t FDCAN2_MESSAGE_RAM_WORD_SIZE = 2560 ; 
+
+#include <ACANFD_STM32.h>
+
+static ACANFD_STM32_FIFO gBuffer ;
+static bool gOk = true ;
+
+uint32_t interval = 3000;//1000; // time between sending out in ms - basically like delay() but doesnt freeze everything
+uint32_t currentMillis, prevMillis = 0;
+
+
+void setup() {
+  gBuffer.initWithSize (100) ;
+  pinMode (LED_BUILTIN, OUTPUT) ;
+  Serial.begin (115200);
+  delay(2000);
+  
+  Serial.println("------------------------------------------------------------------------------");
+  Serial.println();
+  Serial.println("  _  _______  _____ _____  ______ ");
+  Serial.println(" | |/ /_   _|/ ____|  __ \\|  ____|");
+  Serial.println(" | ' /  | | | (___ | |__) | |__   ");
+  Serial.println(" |  <   | |  \\___ \\|  ___/|  __|  ");
+  Serial.println(" | . \\ _| |_ ____) | |    | |____ ");
+  Serial.println(" |_|\\_\\_____|_____/|_|    |______|");
+  Serial.println();
+  Serial.println("------------------------------------------------------------------------------");
+  Serial.println("    CAN FD Read Example");
+  delay(2000);
+  
+  ACANFD_STM32_Settings settings (1000 * 1000, DataBitRateFactor::x4) ;
+  settings.mModuleMode = ACANFD_STM32_Settings::NORMAL_FD; // Found in CANFDMessage.h lines 53 - 58
+
+    const uint32_t errorCode = fdcan1.beginFD (settings) ;
+
+  Serial.print ("Message RAM required minimum size: ") ;
+  Serial.print (fdcan1.messageRamRequiredMinimumSize ()) ;
+  Serial.println (" words") ;
+ 
+ if (0 == errorCode) {
+    Serial.println ("can configuration ok") ;
+  }else{
+    Serial.print ("Error can configuration: 0x") ;
+    Serial.println (errorCode, HEX) ;
+  }
+}
+
+void loop() {
+  //--- Receive frame
+  CANFDMessage receivedFrame ;
+  if (gOk && fdcan1.receiveFD0 (receivedFrame)) {
+    CANFDMessage storedFrame ;
+    Serial.println();
+    Serial.println("------------------------------------------------------------------------------");   
+    Serial.println();    
+    Serial.print("|---ID--|");Serial.print("|---LENGTH---|");Serial.println("|-------------------------DATA------------------------|");
+    Serial.print("   ");Serial.print(receivedFrame.id, HEX);Serial.print("\t");Serial.print("\t");Serial.print(receivedFrame.len);Serial.print("\t");
+    for (int i = 0; i < receivedFrame.len; i++){
+    Serial.print(receivedFrame.data[i]);
+    Serial.print("   ");
+    }
+    Serial.println();
+    Serial.println("------------------------------------------------------------------------------");
+    Serial.println();
+  }
+  
+}// END of Loop()
--- SATLL/STM32H7VIT/can_transmit/can_transmit.ino
+++ SATLL/STM32H7VIT/can_transmit/can_transmit.ino
@@ -16,15 +16,30 @@
 
   Last updated 16/08/2024 Harvey Nixon
 */
-#define CAN1_TX_PIN PD_1
-#define CAN1_RX_PIN PD_0
+#define CAN1_TX_PIN PA12
+#define CAN1_RX_PIN PA11
+
+// #define CAN2_TX_PIN PB13
+// #define CAN2_RX_PIN PB12
 
 static const uint32_t FDCAN1_MESSAGE_RAM_WORD_SIZE = 2560 ;
 static const uint32_t FDCAN2_MESSAGE_RAM_WORD_SIZE = 0 ; // FDCAN2 not used
 
+// static const uint32_t FDCAN1_MESSAGE_RAM_WORD_SIZE = 0 ;// FDCAN1 not used
+// static const uint32_t FDCAN2_MESSAGE_RAM_WORD_SIZE = 2560 ; 
+
 #include <ACANFD_STM32.h>
 
 static ACANFD_STM32_FIFO gBuffer ;
+
+int datatype = 1;
+int counter =0;
+float send_data;
+float pressure = 1120.64;
+float temperature = 21.25;
+float accelerationX = 9.81;
+float accelerationY = -0.12;
+float accelerationZ = 0.10;
 
 static const uint32_t PERIOD = 5000 ;
 static uint32_t gBlinkDate = PERIOD ;
@@ -40,18 +55,93 @@ static uint32_t gExtendedFrameCount = 0 ;
 
 uint32_t interval = 3000;//1000; // time between sending out in ms - basically like delay() but doesnt freeze everything
 uint32_t currentMillis, prevMillis = 0;
-int datatype = 1;
-int counter =0;
-float send_data;
+
 char str_data[8];
 char hex_data[8];
-
-String serin;
-char vel[8];
 
 void floatToCharArray(float value, char* buffer, int bufferSize, int width, int precision){
   dtostrf(value, width, precision, buffer);
 }
+
+void setup() {
+  gBuffer.initWithSize (100) ;
+  pinMode (LED_BUILTIN, OUTPUT) ;
+  Serial.begin (115200);
+  delay(2000);
+  
+  Serial.println("------------------------------------------------------------------------------");
+  Serial.println();
+  Serial.println("  _  _______  _____ _____  ______ ");
+  Serial.println(" | |/ /_   _|/ ____|  __ \\|  ____|");
+  Serial.println(" | ' /  | | | (___ | |__) | |__   ");
+  Serial.println(" |  <   | |  \\___ \\|  ___/|  __|  ");
+  Serial.println(" | . \\ _| |_ ____) | |    | |____ ");
+  Serial.println(" |_|\\_\\_____|_____/|_|    |______|");
+  Serial.println();
+  Serial.println("------------------------------------------------------------------------------");
+  Serial.println("    CAN FD Transmit Example");
+  delay(2000);
+
+  ACANFD_STM32_Settings settings (1000 * 1000, DataBitRateFactor::x4) ;
+  settings.mModuleMode = ACANFD_STM32_Settings::NORMAL_FD; // Found in CANFDMessage.h lines 53 - 58
+
+  const uint32_t errorCode = fdcan1.beginFD (settings) ;
+ if (0 == errorCode) {
+    Serial.println ("can configuration ok") ;
+  }else{
+    Serial.print ("Error can configuration: 0x") ;
+    Serial.println (errorCode, HEX) ;
+  }
+
+}
+
+// class CANMessage {
+//   public : uint32_t id = 0 ;  // Frame identifier
+//   public : bool ext = false ; // false -> standard frame, true -> extended frame
+//   public : bool rtr = false ; // false -> data frame, true -> remote frame
+//   public : uint8_t idx = 0 ;  // This field is used by the driver
+//   public : uint8_t len = 0 ;  // Length of data (0 ... 8)
+//   public : union {
+//     uint8_t  data    [8] = {0, 0, 0, 0, 0, 0, 0, 0} ;
+//   } ;
+
+void loop() {
+  currentMillis = millis();
+
+  if (currentMillis - prevMillis > interval){
+
+  // data
+  switch(datatype){
+    case 1: 
+      send_data = pressure;
+      data();
+      break;
+    case 2:
+      send_data = temperature;
+      data();
+      break;
+    case 3:
+      send_data = accelerationX;
+      data();
+      break;
+    case 4: 
+      send_data = accelerationY;
+      data();
+      break;
+    case 5:
+      send_data = accelerationZ;
+      data();
+      break;
+    case 6:
+    datatype = 0;
+    break;
+    }
+    ++datatype; 
+ 
+      prevMillis = currentMillis;
+  }// END of interval
+
+} // END of Loop()
 
 void data(){
   ++counter;
@@ -59,12 +149,12 @@ void data(){
   Serial.print("Packet: ");Serial.println(counter);
   Serial.print("Data Type: ");Serial.println(datatype);
 
-  //floatToCharArray(send_data, str_data, sizeof(str_data), 8, 6);
-  // for (int i = 0; i < 8 ; i++){
-  //   Serial.print(i);
-  //   Serial.print(" ");
-  //   Serial.println(str_data[i],HEX);
-  //   }
+  floatToCharArray(send_data, str_data, sizeof(str_data), 8, 6);
+  for (int i = 0; i < 8 ; i++){
+    Serial.print(i);
+    Serial.print(" ");
+    Serial.println(str_data[i],HEX);
+    }
 
 
   const uint8_t sendBufferIndex = 0 ;
@@ -82,13 +172,13 @@ void data(){
     Serial.print("Character ");
     Serial.print(i);
     Serial.print(": ");
-    Serial.println(vel[i]);
+    Serial.println(str_data[i]);
 
     }
   Serial.print("Data : ");
   Serial.print("\t");
   for (int i = 0; i < 8 ; i++){
-    Serial.print(vel[i]);
+    Serial.print(str_data[i]);
     Serial.print("\t");
   }
   Serial.println();
@@ -114,15 +204,15 @@ void data(){
         gExtendedFrameCount += 1 ;
       }else{
         gStandardFrameCount += 1 ;
-        frame.id &= 0x1 ;
+        frame.id &= 0x7FFU ;
       }
       switch (frame.type) {
       case CANFDMessage::CAN_DATA :
         gCanDataFrameCount += 1 ;
         //frame.len = 8; // random number between 1 and 8 
-        frame.len = sizeof(vel);
+        frame.len = sizeof(str_data);
         for (uint32_t i=0 ; i<frame.len ; i++) {
-          frame.data[i] = (vel[i]);
+          frame.data[i] = (str_data[i]);
           // this is where the data is 
           // going to create a loop to send data
         }
@@ -138,6 +228,8 @@ void data(){
 
   Serial.println();
   const uint32_t sendStatus = fdcan1.tryToSendReturnStatusFD (frame) ;  
+  //const uint32_t sendStatus = fdcan2.tryToSendReturnStatusFD (frame) ; 
+
 
   Serial.println();      
   Serial.print("|---ID--|");Serial.print("|---LENGTH---|");Serial.println("|-------------------------DATA------------------------|");
@@ -164,83 +256,3 @@ void data(){
   Serial.println();
 
 } //end of data
-
-
-
-
-
-
-void readSerial(){
- 
- if (Serial.available() > 0) {
-    // read the incoming byte:
-    serin = Serial.readString();
-    serin.trim();
-    serin.toCharArray(vel, 8);
-      data();
-  }
-
-
-}
-
-
-
-
-void setup() {
-  gBuffer.initWithSize (100) ;
-  pinMode (LED_BUILTIN, OUTPUT) ;
-  Serial.begin (115200);
-  delay(2000);
-  
-  Serial.println("------------------------------------------------------------------------------");
-  Serial.println();
-  Serial.println("  _  _______  _____ _____  ______ ");
-  Serial.println(" | |/ /_   _|/ ____|  __ \\|  ____|");
-  Serial.println(" | ' /  | | | (___ | |__) | |__   ");
-  Serial.println(" |  <   | |  \\___ \\|  ___/|  __|  ");
-  Serial.println(" | . \\ _| |_ ____) | |    | |____ ");
-  Serial.println(" |_|\\_\\_____|_____/|_|    |______|");
-  Serial.println();
-  Serial.println("------------------------------------------------------------------------------");
-  Serial.println("    CAN FD Transmit Example");
-  delay(2000);
-
-    ACANFD_STM32_Settings settings (1000 * 1000, DataBitRateFactor::x4) ;
-  settings.mModuleMode = ACANFD_STM32_Settings::NORMAL_FD; // Found in CANFDMessage.h lines 53 - 58
-
-  const uint32_t errorCode = fdcan1.beginFD (settings) ;
- if (0 == errorCode) {
-    Serial.println ("can configuration ok") ;
-  }else{
-    Serial.print ("Error can configuration: 0x") ;
-    Serial.println (errorCode, HEX) ;
-  }
-
-
-
-
-}
-
-void loop() {
-  readSerial();
-
-CANFDMessage receivedFrame ;
-  if (gOk && fdcan1.receiveFD0 (receivedFrame)) {
-    CANFDMessage storedFrame ;
-    Serial.println();
-    Serial.println("------------------------------------------------------------------------------");   
-    Serial.println();    
-    Serial.print("|---ID--|");Serial.print("|---LENGTH---|");Serial.println("|-------------------------DATA------------------------|");
-    Serial.print("   ");Serial.print(receivedFrame.id, HEX);Serial.print("\t");Serial.print("\t");Serial.print(receivedFrame.len);Serial.print("\t");
-    for (int i = 0; i < receivedFrame.len; i++){
-    Serial.print(receivedFrame.data[i]);
-    Serial.print("   ");
-    }
-    Serial.println();
-    Serial.println("------------------------------------------------------------------------------");
-    Serial.println();
-  }
-
-
-
-}
